d780d635fd32947b2eae14faaa955b10f3b108a1
Bensen Fan <bensenfan88@gmail.com>
2015-09-30 19:41:39 -0700

    bensen's extra work for week01/day03

---
+++ 01-Week/03-day/advance_js_lab.js
@@ -0,0 +1,23 @@
+/*Hi Everyone! As I mentioned, for those of you who are done with 
+  the javascript lab as well as last night's homework, both the
+  layout exercise and the form exercise, here's a little extra work
+  for tonight. This is very challenging and likely will need concepts
+  we have not yet covered. If you have time, give it a shot! 
+  
+  Basically, we are creating a function that checks if a string
+  and any of its anagrams is a palindrome. So, if the string is 
+  'caa', which contains the anagram 'aca', the function would 
+  return true. */
+
+//Here's something to get you started. 
+
+function canBePalindrome(str){
+	var answer; 
+
+	/*YOUR CODE HERE*/
+
+	return answer
+}
+
+canBePalindrome('caa') // true
+canBePalindrom('bensen') // false
