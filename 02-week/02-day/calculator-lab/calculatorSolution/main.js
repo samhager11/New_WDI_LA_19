5f69961b661488634270c947e333058b16b4aab8
Tony <david.guerrero87@live.com>
2015-10-06 12:23:53 -0700

    global variables complete

--- 02-week/02-day/calculator-lab/calculatorSolution/main.js
+++ 02-week/02-day/calculator-lab/calculatorSolution/main.js
@@ -1,1 +1,9 @@
 console.log("--------Script Started----------");
+//Global Variables
+var result ,
+    inputValues = [],
+    operator,
+    buttons = document.getElementsByClassName('buttons');
+
+//show the variables in the console
+console.log("Result:"+ result, "Input Values:"+inputValues,"Operator:"+operator,buttons);
