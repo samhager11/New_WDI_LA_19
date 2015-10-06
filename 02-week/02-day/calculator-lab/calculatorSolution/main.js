1a5b2a76259fcba9d6f0b3a069752377f23dbedf
Tony <david.guerrero87@live.com>
2015-10-06 12:48:21 -0700

    some js functionality

--- 02-week/02-day/calculator-lab/calculatorSolution/main.js
+++ 02-week/02-day/calculator-lab/calculatorSolution/main.js
@@ -3,7 +3,25 @@ console.log("--------Script Started----------");
 var result ,
     inputValues = [],
     operator,
-    buttons = document.getElementsByClassName('buttons');
+    buttons = document.getElementsByClassName('buttons'),
+    inputBox = document.getElementById('display');
 
 //show the variables in the console
 console.log("Result:"+ result, "Input Values:"+inputValues,"Operator:"+operator,buttons);
+
+//create our basic functions
+function clear(){
+  result = "";
+  inputValues = [];
+  operator = "";
+  inputBox.value = "";
+}
+
+// add event listener to all buttons
+for(i=0; i<buttons.length; i++)
+{
+  buttons[i].addEventListener('click',function(){
+    inputValues.push(this.innerHTML);
+    console.log("Current values stored:",inputValues);
+  });
+}
