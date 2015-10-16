649b07d4df8792080e56732b6014d90f07c1a14f
Sam Hager <sam.hager11@gmail.com>
2015-10-15 20:37:36 -0700

    added rest of question set

--- projects/project-01/project1_files/code_files/script.js
+++ projects/project-01/project1_files/code_files/script.js
@@ -3,25 +3,59 @@
 //Define questions to be passed into Category object constructor. The index of each variable's array is used in the Category object constructor and should follow the syntax as var newQuestion = ['questionStatement','answerStatement',['falseAnswer1','falseAnswer2','faleseAnswer3']];
 
 //Get it and Set it Category questions
-var getItQuestion1 = ['Which jQuery method can get or set a specified attribute and its value?','.attr()',['.val()','.addAttr()','.element()']];
+var getItQuestion1 = ['Which jQuery method can get or set a specified attribute and its value?','.attr()',['.val()','.html()','.NSA()']];
 
-var getItQuestion2 = ['Which jQuery method removes a specified attribute and its value?','.removeAttr()',['.remove()','.changeAttr()','.toggleAttr()']];
+var getItQuestion2 = ['Which jQuery method removes a specified attribute and its value?','.removeAttr()',['.bankrupt()','.changeAttr()','.loseMoney()']];
 
-var getItQuestion3 = ['Which jQuery method adds a new value to the existing value of the class attribute?','.addClass()',['.class()','.val()','.classVal()']];
+var getItQuestion3 = ['Which jQuery method adds a new value to the existing value of the class attribute?','.addClass()',['.movinOnUp()','.val()','.getMoney()']];
+
+var getItQuestion4 = ['Which jQuery method allows you to retrieve the html content of a jQuery selection?','.html()',['.text()','.val()','.goldenRetriever()']];
+
+//text
+var getItQuestion5 = ['Which jQuery method allows you to give the same new text content to every element of a selection?','.text()',['.paint','.content()','.ditto()']];
+
+//append
+var getItQuestion6 = ['Which method would you use to insert specified content at the end of the selected elements?','.append()',['.pinIt()','.prepend()','.attach()']];
+//replaceWith
+var getItQuestion7 = ['Which jQuery method replaces every element in a selection with new content?','.replaceWith()',['.content()','.addContent()','.switchContent()']];
+//remove
+var getItQuestion8 = ['Which jQuery method adds a new value to the existing value of the class attribute?','.addClass()',['.payDay()','.val()','.upUrGame()']];
+
+
 
 //Event Listener Category questions - not completed
-var eventQuestion1 = ['Test question 1?','answer Q1',['falseAnswer1 Q1','falseAnswer2 Q1','falseAnswer3 Q1']];
+var eventQuestion1 = ['Which jQuery method is used to handle events?','.on()',['.listen()','.eventListener()','.theBouncer()']];
 
-var eventQuestion2 = ['Test question 2?','answer Q2',['falseAnswer1 Q2','falseAnswer2 Q2','falseAnswer3 Q2']];
+var eventQuestion2 = ['What is the first parameter that .on() accepts?','event',['yourNameHere','attribute','point']];
 
-var eventQuestion3 = ['Test question 3?','answer Q3',['falseAnswer1 Q3','falseAnswer2 Q3','falseAnswer3 Q3']];
+var eventQuestion3 = ['What is the second parameter that .on() accepts?','code',['listener','fleek','element']];
+
+var eventQuestion4 = ['Which event listener responds to a click trigger?','click',['thePopo','mouseon','gameProper']];
+
+var eventQuestion5 = ['Which event will run when the browser is loaded?','ready',['waiting','popups','netflix']];
+
+var eventQuestion6 = ['Which event will listen for (and bind an event on) a key being pressed down?','keypress',['tap','touch','sausage fingers']];
+
+var eventQuestion7 = ['Which event object property returns the DOM element that initiated the event?','target',['data','type','Vin Diesel']];
+
+var eventQuestion8 = ['How many beers has Tony had today?','All of them',['just one','zero','false']];
 
 //Finding Elements Category questions - not completed
-var findQuestion1 = ['Test question 1?','answer Q1',['falseAnswer1 Q1','falseAnswer2 Q1','falseAnswer3 Q1']];
+var findQuestion1 = ['Which jQuery method allows you to find the nearest ancestor (not just parent) that matches the selector?','.closest()',['.familyTree()','.stepCousin()','.ancestor()']];
 
-var findQuestion2 = ['Test question 2?','answer Q2',['falseAnswer1 Q2','falseAnswer2 Q2','falseAnswer3 Q2']];
+var findQuestion2 = ['Which method will select the direct parent of a selection?','.parent()',['.rents()','.above()','.grounded()']];
 
-var findQuestion3 = ['Test question 3?','answer Q3',['falseAnswer1 Q3','falseAnswer2 Q3','falseAnswer3 Q3']];
+var findQuestion3 = ['Which method will find all of the siblings of the current selection?','.siblings()',['.brothers()','.sisters()','.favoriteChild()']];
+
+var findQuestion4 = ['Which method will grab the next sibling of the current element?','.next()',['.theWeakestLink()','.headlock()','.sibling()']];
+
+var findQuestion5 = ['Which method lets you filter elements from a second selector?','.filter()',['.twoTimer()','.second()','.brita()']];
+
+var findQuestion6 = ['Which method finds descendants of elements that match the selector?','.find()',['.search()','.familyHistory()','.junior()']];
+
+var findQuestion7 = ['Which method checks whether the current selection matches a condition?','.is()',['.theDoctor()','.boolean()','.rogerChecking()']];
+
+var findQuestion8 = ['Who is the best GA cohort in all the land?','WDI LA 19',['theOtherGuys','18isClose','youAlreadyKnow']];
 
 
 
@@ -93,7 +127,7 @@ var counterResetTime = 5;
 
 //This Category Object Constructor allows a new category with a set of questions to be easily created. The questions (with corresponding answers) should be defined above in variables as exhibited by 'getItQuestion1'. At the current time, each category object constructor is able to accomodate for three(3) questions. The constructor would need to be modified to accomodate additional questions per each category. The constructor takes four(4) arguments - a category name (nameIt), and three questions (insert questions from variables defined above).
 
-var Category = function(nameIt,q1,q2,q3){
+var Category = function(nameIt,q1,q2,q3,q4,q5,q6,q7,q8){
   this.catName = nameIt;
   this.Questions = [{
       question: q1[0],
@@ -105,19 +139,34 @@ var Category = function(nameIt,q1,q2,q3){
     },{question: q3[0],
         answer: q3[1],
         falseAnswers: q3[2],
+    },{question: q4[0],
+      answer: q4[1],
+      falseAnswers: q4[2],
+    },{question: q5[0],
+      answer: q5[1],
+      falseAnswers: q5[2],
+    },{question: q6[0],
+      answer: q6[1],
+      falseAnswers: q6[2],
+    },{question: q7[0],
+      answer: q7[1],
+      falseAnswers: q7[2],
+    },{question: q8[0],
+      answer: q8[1],
+      falseAnswers: q8[2],
     }]
   }
 
 
 //Creates new Category objects with Questions and Answers from Category object constructor found above on lines 74 through 86. MAX OF 3 QUESTIONS CURRENTLY BASED ON CATEGORY OBJECT CONSTRUCTOR ////////
 
- getItCategory = new Category('Get It and Set It',getItQuestion1,getItQuestion2,getItQuestion3);
+ getItCategory = new Category('Get It and Set It',getItQuestion1,getItQuestion2,getItQuestion3,getItQuestion4,getItQuestion5,getItQuestion6,getItQuestion7,getItQuestion8);
 
- eventCategory = new Category('Welcome to the Big Event',eventQuestion1,eventQuestion2,eventQuestion3);
+ eventCategory = new Category('Welcome to the Big Event',eventQuestion1,eventQuestion2,eventQuestion3,eventQuestion4,eventQuestion5,eventQuestion6,eventQuestion7,eventQuestion8);
 
- searchCategory = new Category('Element Search Party',findQuestion1,findQuestion2,findQuestion3);
+ findCategory = new Category('Element Search Party',findQuestion1,findQuestion2,findQuestion3,findQuestion4,findQuestion5,findQuestion6,findQuestion7,findQuestion8);
 
-categoryArray.push(getItCategory,eventCategory,searchCategory);
+categoryArray.push(getItCategory,eventCategory,findCategory);
 
 ///////////////////////////////////////////////////////////////////
 
